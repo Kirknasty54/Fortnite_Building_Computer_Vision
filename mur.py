6cce1408d1a5a7ba7b63d5c68ecbac0187a32d74
Jackson Kirkpatrick <Kirkpatrick545454@gmail.com>
2024-03-17 19:50:29 -0500

    Added full hand recognition and shape detection functionality. If a hand is in frame, no contour detection will occur, and once a hand is out of frame, then image detection will begin. Next is just figuring out a way to tally the shapes on the whiteboard and do appropriate number of actions.

--- mur.py
+++ mur.py
@@ -24,16 +24,17 @@ cv2.createTrackbar("Threshold1", "Parameters", 44, 255, empty)
 cv2.createTrackbar("Threshold2", "Parameters", 255, 255, empty)
 
 
-#this finds the contours of any images in the frame
-def getContours(img, img_contour):
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+#this finds the contours of any images in the frame, this function will only occur if a hand is not detected
+def getContours(img_dil, img_contour, img):
+    contours, hierarchy = cv2.findContours(img_dil, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     #draws contours for objects within a certain area, we will probably need to tweak this so it doesnt include whiteboard if
     # the whiteboards edges are in frames
     for cnt in contours:
         area = cv2.contourArea(cnt)
         if area > 1000:
-            cv2.drawContours(img_contour, cnt, -1, (255, 0, 255), 7)
+            cv2.drawContours(img, cnt, -1, (255, 0, 255), 7)
+            cv2.drawContours(img_dil, cnt, -1, (255, 0, 255), 7)
             perimeter = cv2.arcLength(cnt, True)
             approx = cv2.approxPolyDP(cnt, 0.02 * perimeter, True)
             print(len(approx))
@@ -45,23 +46,22 @@ while True:
     img_copy = img.copy()
     img_copy = detector.find_hands(img)
     landmark_list = detector.find_pos(img)
-    if len(landmark_list) != 0:
-        cv2.imshow("Result", img)
-        print(landmark_list[4])
-    else:
-        img_blur = cv2.GaussianBlur(img, (7, 7), 1)
-        img_gray = cv2.cvtColor(img_blur, cv2.COLOR_BGR2GRAY)
-        thresehold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
-        thresehold2 = cv2.getTrackbarPos("Threshold1", "Parameters")
+    cv2.imshow("Result", img)
 
-        img_canny = cv2.Canny(img_gray, thresehold1, thresehold2)
-        kernel = np.ones((5, 5))
-        img_dil = cv2.dilate(img_canny, kernel, iterations=1)
+    img_blur = cv2.GaussianBlur(img, (7, 7), 1)
+    img_gray = cv2.cvtColor(img_blur, cv2.COLOR_BGR2GRAY)
+    thresehold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
+    thresehold2 = cv2.getTrackbarPos("Threshold1", "Parameters")
 
-        getContours(img_dil, img_contour)
-        img_stack = stackImages(0.8, ([img, img_gray, img_canny],
-                                    [img_dil, img_contour, img_contour]))
+    img_canny = cv2.Canny(img_gray, thresehold1, thresehold2)
+    kernel = np.ones((5, 5))
+    img_dil = cv2.dilate(img_canny, kernel, iterations=1)
 
-        cv2.imshow("Result", img_stack)
-        if cv2.waitKey(1) & 0xff == ord("q"):
-            break
+    if len(landmark_list) == 0:
+        getContours(img_dil, img_contour, img)
+    #img_stack = stackImages(0.8, ([img, img_gray, img_canny],
+    #                            [img_dil, img_contour, img_contour]))
+
+    cv2.imshow("Result", img)
+    if cv2.waitKey(1) & 0xff == ord("q"):
+        break
