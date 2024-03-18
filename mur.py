85eec2525c5c784935fa017b2f9477a989e98b54
Jackson Kirkpatrick <Kirkpatrick545454@gmail.com>
2024-03-18 17:26:51 -0500

    Finalized some shape detection. The code 'works' but the Fortnite anti-cheat prevents from builds being confirmed. Will try and figure a work around

--- mur.py
+++ mur.py
@@ -1,8 +1,12 @@
 #import all needed modules, including custom created one
 import cv2
 import numpy as np
+import pynput.keyboard
+
 from stack import stackImages
 import HandTrackingModule as htm
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Key, Controller
 
 #create window with specified width/height
 frame_width = 640
@@ -11,6 +15,9 @@ frame_height = 480
 cap = cv2.VideoCapture(0)
 #initalize our HandDetector
 detector = htm.HandDetector()
+mouse = pynput.mouse.Controller()
+keyboard = pynput.keyboard.Controller()
+
 cap.set(3, frame_width)
 cap.set(4, frame_height)
 
@@ -26,6 +33,7 @@ cv2.createTrackbar("Threshold2", "Parameters", 255, 255, empty)
 
 #this finds the contours of any images in the frame, this function will only occur if a hand is not detected
 def getContours(img_dil, img_contour, img):
+    shapes = []
     contours, hierarchy = cv2.findContours(img_dil, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     #draws contours for objects within a certain area, we will probably need to tweak this so it doesnt include whiteboard if
@@ -37,10 +45,40 @@ def getContours(img_dil, img_contour, img):
             cv2.drawContours(img_dil, cnt, -1, (255, 0, 255), 7)
             perimeter = cv2.arcLength(cnt, True)
             approx = cv2.approxPolyDP(cnt, 0.02 * perimeter, True)
-            print(len(approx))
-
+            x, y, w, h = cv2.boundingRect(cnt)
+            #use aspect ratio to determine the type of rectangle detected, be it horizontal or vertical
+            aspect_ratio = float(w) / h
+            if(len(approx) == 3):
+                print('triangle found, l will need to be pressed')
+                keyboard.press('v')
+                #keyboard.press('f')
+                #keyboard.release('f')
+                keyboard.release('v')
+                #mouse.click(Button.left, 1)
+                #mouse.release(Button.left)
+            elif(len(approx) == 4):
+                if aspect_ratio > 1.5:
+                    print('horizontal rectangle found, k')
+                    keyboard.press('x')
+                    #keyboard.press('f')
+                    #keyboard.release('f')
+                    keyboard.release('x')
+                    #mouse.click(Button.left, 1)
+                    #mouse.release(Button.left)
+                elif aspect_ratio < 0.5:
+                    print('vertical rectangle found, j')
+                    keyboard.press('z')
+                    #keyboard.press('f')
+                    #keyboard.release('f')
+                    keyboard.release('z')
+                    #mouse.click(Button.left, 1)
+                    #mouse.release(Button.left)
+            #print(len(approx))
 
 while True:
+    #read in the camera data and do some processing/filtering to get more accurate results for the corresponding task
+    #the shape detection and hand detection algorithms use different kinds of filtering, may try and figure out a way to combine these to see if it can become
+    #more efficient
     success, img = cap.read()
     img_contour = img.copy()
     img_copy = img.copy()
@@ -57,11 +95,15 @@ while True:
     kernel = np.ones((5, 5))
     img_dil = cv2.dilate(img_canny, kernel, iterations=1)
 
+    #if no landmarks are detected, then shape detection will occur, otherwise we continue with hand recognition and drawing the hands to indicate
+    #shape detection hasn't started yet
     if len(landmark_list) == 0:
         getContours(img_dil, img_contour, img)
+    #this img_stack variable was used for getting a better view on the contour processing of the image, its not really needed in this context, but its a nice thing to have regardless
     #img_stack = stackImages(0.8, ([img, img_gray, img_canny],
     #                            [img_dil, img_contour, img_contour]))
 
     cv2.imshow("Result", img)
+    #input q to terminate program and close windows
     if cv2.waitKey(1) & 0xff == ord("q"):
         break
